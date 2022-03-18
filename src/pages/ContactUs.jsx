a9ac953d3e4395186e05f977f7d211a1ca28086d
snehal <mahabaresnehal@gmail.com>
2022-03-19 01:39:14 +0530

    contact page error resolved

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -19,7 +19,7 @@ function ContactUs() {
                             <div className="bs-section">
                                 <div className="sec-cont">
                                     <div className="mod-map">
-                                        {/* <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Bus%20Stop,%20Sr.%20No%20237/2,%20Behind%20Ranjai%20Bunglow,%20Aloma%20County%20Road,%20near%20Medipoint%20Hospital,%20Aundh,%20Pune,%20Maharashtra%20411007+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/">navigation gps</a></iframe></div> */}
+                                        <div style={{width: "100%"}}><iframe width="100%" height="600" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Bus%20Stop,%20Sr.%20No%20237/2,%20Behind%20Ranjai%20Bunglow,%20Aloma%20County%20Road,%20near%20Medipoint%20Hospital,%20Aundh,%20Pune,%20Maharashtra%20411007+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/marine-gps/">navigation gps</a></iframe></div>
                                     </div>
                                 </div>
                             </div>
@@ -97,7 +97,7 @@ function ContactUs() {
                                                 <input type="text" className="form-control" placeholder="verify" />
                                             </div>
                                             <div className="form-group">
-                                                <input type="text" readonly className="form-control typ-readonly" value="294040"  />
+                                                <input type="text" readOnly className="form-control typ-readonly" value="294040"  />
                                             </div>
                                             <div className="form-group typ-full">
                                                 <textarea rows="8" cols="50" className="form-control" placeholder="message"></textarea>
