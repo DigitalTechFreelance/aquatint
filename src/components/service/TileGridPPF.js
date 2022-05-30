4ab37832d612d64bb36ec2cbb07737ecf41e252a
Darshan Shah <darshan@digitalsalt.in>
2022-05-30 11:11:32 +0530

    unwanted code removed

--- src/components/service/TileGridPPF.js
+++ src/components/service/TileGridPPF.js
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
-import washImg from '../../assets/images/offering-wash.webp';
-import detailImg from '../../assets/images/offering-detail.webp';
-import ceramicCoatingImg from '../../assets/images/offering-ceramic-coating.webp';
-import ppfImg from '../../assets/images/offering-ppf.webp';
-import customizeImg from '../../assets/images/offering-customize.webp'; 
 import useSetBgImage from '../../hooks/useSetBgImage';
 import ReactPlayer from 'react-player';
 
@@ -22,7 +17,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ3">
                     <div className="img-wrap">
-                        {/* <img src={washImg} alt="Wash Service" title="wash Service" className="getBgSrc" /> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=8k41mZZY2tQ' 
                             loop={true}
@@ -42,7 +36,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ4">
                     <div className="img-wrap">
-                        {/* <img src={detailImg} alt="Detail Service" title="Detail Service" className="getBgSrc"/> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=8k41mZZY2tQ' 
                             loop={true}
@@ -62,7 +55,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ5">
                     <div className="img-wrap">
-                        {/* <img src={customizeImg} alt="Ceramic Coating" title="Ceramic Coating" className="getBgSrc"/> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=j_GmoXQTB2c' 
                             loop={true}
@@ -81,7 +73,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ3">
                     <div className="img-wrap">
-                        {/* <img src={ceramicCoatingImg} alt="Ceramic Coating" title="Ceramic Coating" className="getBgSrc"/> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=biYnXvwlqVM' 
                             loop={true}
@@ -101,7 +92,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ4">
                     <div className="img-wrap">
-                        {/* <img src={ppfImg} alt="Paint Protection Film" title="Paint Protection Film" className="getBgSrc"/> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=R-PQLRLjFco' 
                             loop={true}
@@ -121,7 +111,6 @@ function TileGridPPF() {
             <Grid item xs={12} md={4} className="grid-item">
                 <div className="bs-tile typ5">
                     <div className="img-wrap">
-                        {/* <img src={customizeImg} alt="Customise" title="Customise" className="getBgSrc"/> */}
                         <ReactPlayer 
                             url='https://www.youtube.com/watch?v=8k41mZZY2tQ' 
                             loop={true}
