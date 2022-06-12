b759f49d3984a8faeb5e69f22168da7a733b9632
Darshan Shah <darshan@digitalsalt.in>
2022-06-12 18:18:20 +0530

    ui fixes done

--- src/components/home/TileGrid.js
+++ src/components/home/TileGrid.js
@@ -24,15 +24,6 @@ function TileGrid() {
                     <div className="bs-tile typ1 setBgSrc">
                         <div className="img-wrap">
                             <img src={washImg} alt="Wash Service" title="wash Service" className="getBgSrc" />
-                            {/* <VideoPlayer
-                                buttonClose={false}
-                                srcUrl={'https://www.youtube.com/v/8k41mZZY2tQ?version=2'}
-                                muted={true}
-                                autoPlay={true}
-                                className="mod-gif"
-                                height={window.innerWidth >= 767 ? "40.8rem" : "auto"}
-                                loop={true}
-                            /> */}
                         </div>
                         <div className="text-wrap">
                             <h3 className="title">wash</h3>
@@ -44,15 +35,6 @@ function TileGrid() {
                     <div className="bs-tile typ2 setBgSrc">
                         <div className="img-wrap">
                             <img src={detailImg} alt="Detail Service" title="Detail Service" className="getBgSrc" />
-                            {/* <VideoPlayer
-                                buttonClose={false}
-                                srcUrl={'https://www.youtube.com/v/Cdd_amqSFAg?version=2'}
-                                muted={true}
-                                autoPlay={true}
-                                className="mod-gif"
-                                height={window.innerWidth >= 767 ? "40.8rem" : "auto"}
-                                loop={true}
-                            /> */}
                         </div>
                         <div className="text-wrap">
                             <h3 className="title">detail</h3>
@@ -64,15 +46,6 @@ function TileGrid() {
                     <div className="bs-tile typ3 setBgSrc">
                         <div className="img-wrap">
                             <img src={ceramicCoatingImg} alt="Ceramic Coating" title="Ceramic Coating" className="getBgSrc" />
-                            {/* <VideoPlayer
-                                buttonClose={false}
-                                srcUrl={'https://www.youtube.com/v/CXWX0U3EXqk?version=2'}
-                                muted={true}
-                                autoPlay={true}
-                                className="mod-gif"
-                                height={window.innerWidth >= 767 ? "40.8rem" : "auto"}
-                                loop={true}
-                            /> */}
                         </div>
                         <div className="text-wrap">
                             <h3 className="title">ceramic coating</h3>
@@ -85,15 +58,6 @@ function TileGrid() {
                     <div className="bs-tile typ4 setBgSrc">
                         <div className="img-wrap">
                             <img src={ppfImg} alt="Paint Protection Film" title="Paint Protection Film" className="getBgSrc" />
-                            {/* <VideoPlayer
-                                buttonClose={false}
-                                srcUrl={'https://www.youtube.com/v/8BqXWxXRS4E?version=2'}
-                                muted={true}
-                                autoPlay={true}
-                                className="mod-gif"
-                                height={window.innerWidth >= 767 ? "40.8rem" : "auto"}
-                                loop={true}
-                            /> */}
                         </div>
                         <div className="text-wrap">
                             <h3 className="title">paint protection film</h3>
@@ -105,15 +69,6 @@ function TileGrid() {
                     <div className="bs-tile typ5 setBgSrc">
                         <div className="img-wrap">
                             <img src={customizeImg} alt="Customise" title="Customise" className="getBgSrc" />
-                            {/* <VideoPlayer
-                                buttonClose={false}
-                                srcUrl={'https://www.youtube.com/v/9sBozUaYajE?version=2'}
-                                muted={true}
-                                autoPlay={true}
-                                className="mod-gif"
-                                height={window.innerWidth >= 767 ? "40.8rem" : "auto"}
-                                loop={true}
-                            /> */}
                         </div>
                         <div className="text-wrap">
                             <h3 className="title">cutomise</h3>
