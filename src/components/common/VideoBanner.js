1a558e38e9d6c7692c9ce398441f76b3d13a0ac1
Snehalmm <mahabaresnehal@gmail.com>
2022-12-26 11:11:34 +0530

    home banner added

--- src/components/common/VideoBanner.js
+++ src/components/common/VideoBanner.js
@@ -21,30 +21,38 @@ function HomeBanner({ data }) {
     });
   };
 
-  const { title, desktopVideoUpload, mobileVideoUpload, desktopYoutubeLink, mobileYoutubeLink } = data && data
-  const desktopVideoUrl = desktopVideoUpload !== null ? desktopVideoUpload?.data?.url : JSON.parse(desktopYoutubeLink).url
-  const mobileVideoUrl = mobileVideoUpload !== null ? mobileVideoUpload?.data?.url : JSON.parse(mobileYoutubeLink).url
-
+  const { title, desktopVideoUpload, mobileVideoUpload, desktopYoutubeLink, mobileYoutubeLink, desktopBannerImage, mobileBannerImage } = data && data
+  const desktopVideoUrl = desktopVideoUpload !== null ? desktopVideoUpload?.data?.url : JSON.parse(desktopYoutubeLink)?.url
+  const mobileVideoUrl = mobileVideoUpload !== null ? mobileVideoUpload?.data?.url : JSON.parse(mobileYoutubeLink)?.url
   return (
-    <div className="bs-banner">
-      <div className="video-wrap">
-        <VideoPlayer
-          buttonClose={false}
-          playingStatus={handleVideoStatus}
-          srcUrl={size.width > 768 ? desktopVideoUrl : mobileVideoUrl}
-          muted={true}
-          autoPlay={true}
-          className="video-player"
-          height="100%"
-          loop={true}
-        />
-      </div>
+    <div 
+    className={`bs-banner ${desktopVideoUrl && mobileVideoUrl}? 'video-cont': '' `}
+     >
+      {desktopVideoUrl && mobileVideoUrl ? (
+        <div className="video-wrap">
+          <VideoPlayer
+            buttonClose={false}
+            playingStatus={handleVideoStatus}
+            srcUrl={size.width > 768 ? desktopVideoUrl : mobileVideoUrl}
+            muted={true}
+            autoPlay={true}
+            className="video-player"
+            height="100%"
+            loop={true}
+          />
+        </div>
+      ) : (
+        <div className='img-wrap'>
+          <img src={size.width > 768 ? desktopBannerImage?.url : mobileBannerImage?.url} alt="Coating Car" title="Coating Car" />
+        </div>
+      )}
+
       <div className="banner-info">
         <Fade bottom distance="20px" delay={800} duration={800}>
           <h2 className="banner-title">{title}</h2>
         </Fade>
       </div>
-    </div>
+    </div >
   )
 }
 
