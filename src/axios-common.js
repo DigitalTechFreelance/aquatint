7951611bace5ee2100e9426b5d594d2c36e8548c
Santosh Dawange <santosh.dawange07@gmail.com>
2024-09-06 13:52:16 +0530

    gtm code added

--- src/axios-common.js
+++ src/axios-common.js
@@ -1,12 +1,12 @@
 import axios from 'axios'; 
 
 
-export const baseURL = 'https://uat-aquatint-strapi.link/'
+export const baseURL = 'https://aquatint-strapi-v3.cloudjiffy.net/'
 // export const baseURL = 'http://localhost:1337/'
 
 let defaultHeaders = {
         'Accept': 'application/json',
-        'Content-Type': 'application/json' ,
+        'Content-Type': 'application/json',
     }
 
 const instance = axios.create({
